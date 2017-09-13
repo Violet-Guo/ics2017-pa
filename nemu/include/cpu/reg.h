a8de581a8f7734707abb80832cbddce790d254d9
tracer-ics2017 <tracer@njuics.org>
2017-09-13 18:24:39 +0200

    >  compile 0000007 vagrant Linux localhost.localdomain 3.10.0-229.el7.x86_64 #1 SMP Fri Mar 6 11:36:42 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:24:39 up  4:42,  2 users,  load average: 0.00, 0.03, 0.05 37e2bdd746641a2c4f0c74c7df7023135d12aa2

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,7 +15,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-  struct {
+  union {
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
@@ -26,7 +26,9 @@ typedef struct {
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	struct{
+	  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	};
 
   vaddr_t eip;
 
