7b3dfbbb322002aa0d1748c58b2f89637ba8d6f3
Violet-Guo <605654408@qq.com>
2017-09-18 18:08:41 +0200

    >  compile 0000007 vagrant Linux localhost.localdomain 3.10.0-229.el7.x86_64 #1 SMP Fri Mar 6 11:36:42 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:08:41 up  1:02,  2 users,  load average: 0.00, 0.01, 0.05 7c2a2c1fd2c4f239d0d9f93527b1c671673f29ef

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -67,6 +67,7 @@ static bool make_token(char *e) {
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
+      printf("in the for\n");
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
 	printf("in the if\n");
         char *substr_start = e + position;
