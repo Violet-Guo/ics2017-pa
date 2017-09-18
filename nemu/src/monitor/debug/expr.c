e6bc23295234bfe4de65c3cd8e35213af815a743
Violet-Guo <605654408@qq.com>
2017-09-18 18:31:43 +0200

    >  compile 0000007 vagrant Linux localhost.localdomain 3.10.0-229.el7.x86_64 #1 SMP Fri Mar 6 11:36:42 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:31:43 up  1:26,  2 users,  load average: 0.08, 0.03, 0.05 c1a8f87483244b2118e90184247195d74e70c1e4

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -68,10 +68,6 @@ static bool make_token(char *e) {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       printf("in the for\n");
-      //printf("regexec = %d\npmatch.rm_so = %d\n", regexec(&re[i], e + position, 1, &pmatch, 0), pmatch.rm_so);
-      //printf("%d\n", pmatch.rm_so);
-      //printf("%d, %d\n", regexec(&re[i], e + position, 1, &pmatch, 0) == 0, pmatch.rm_so == 0);
-      //printf("%d\n", pmatch.rm_so);
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
 	printf("in the if\n");
         char *substr_start = e + position;
