65a2fb58e07d5d53db2452e828e8955c9a6769af
tracer-ics2017 <tracer@njuics.org>
2017-09-18 18:35:36 +0200

    >  compile 0000007 vagrant Linux localhost.localdomain 3.10.0-229.el7.x86_64 #1 SMP Fri Mar 6 11:36:42 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:35:36 up  1:29,  2 users,  load average: 0.03, 0.04, 0.05 a9ff41e185d51a8f4a60dbec2091afcadcf54b1f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,7 +7,7 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
+  TK_NOTYPE = 256, TK_EQ, NUM
 
   /* TODO: Add more token types */
 
@@ -24,7 +24,8 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"==", TK_EQ},        // equal
+  {"[0-9]+", NUM}        // numbers
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
