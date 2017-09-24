69b98edc3fa6ef4f4309ef07491411ff75019f73
Violet-Guo <605654408@qq.com>
2017-09-24 09:15:14 +0200

    >  compile 0000007 vagrant Linux localhost.localdomain 3.10.0-229.el7.x86_64 #1 SMP Fri Mar 6 11:36:42 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  09:15:14 up  1:40,  2 users,  load average: 0.00, 0.01, 0.05 fdf2fa7239e823f0f420df212a007c63515fc2ce

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -172,6 +172,7 @@ uint32_t eval(int p, int q) {
     int op = 1;
     uint32_t val1 = eval(p, op - 1);
     uint32_t val2 = eval(op + 1, q);
+    printf("val1 = %u\n, val2 = %u\n", val1, val2);
 
     switch (op) {
       case ADD:
