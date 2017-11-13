9d069a0e46a47bbb0f34ee558701f6c3a37f956d
Violet-Guo <605654408@qq.com>
2017-11-13 05:20:57 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  05:20:57 up  7:19,  3 users,  load average: 0.00, 0.00, 0.00 20a756087557772d790a94deec89cec92f2a218

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -131,7 +131,7 @@ static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  TODO();
+	*dest = ~(*dest);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
