3e8e03985e13ee1cdc7abeb21d315fcb9b899dd8
Violet-Guo <605654408@qq.com>
2017-11-14 11:49:10 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  11:49:10 up 26 min,  3 users,  load average: 0.00, 0.02, 0.00 e695cbec7f1a161dfe62f3427c82a1c52c4e76f

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -16,12 +16,12 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   switch (subcode & 0xe) {
     case CC_O:
     case CC_B:
-    case CC_E:
-    case CC_BE:
+    case CC_E:  *dest = cpu.eflags.CF;  break;
+    case CC_NE: *dest = !cpu.eflags.CF;  break;
+		case CC_BE:
     case CC_S:
     case CC_L:
     case CC_LE:
-      TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
