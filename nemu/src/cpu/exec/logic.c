588689cb4847ae857dbeb50557a56f68258e9b57
tracer-ics2017 <tracer@njuics.org>
2017-11-14 12:18:07 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:18:07 up 55 min,  3 users,  load average: 0.00, 0.00, 0.00 b168094b2dfeec07cbc014a1aeaa0014e6be47c0

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -1,8 +1,10 @@
 #include "cpu/exec.h"
 
 make_EHelper(test) {
-  TODO();
-
+  rtl_and(&t2, &id_dest->val, &id_src->val);
+  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
   print_asm_template2(test);
 }
 
