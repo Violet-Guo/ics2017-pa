dc6844ce76b17d2efdb1b384e2032715fe07068c
violet <guojing@ict.ac.cn>
2017-11-15 06:00:15 +0000

    change

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -198,8 +198,8 @@ static inline void rtl_update_SF(const rtlreg_t* result, int width) {
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
-  rtl_update_ZF(result, width);
-  rtl_update_SF(result, width);
+	rtl_update_ZF(result, width);
+	rtl_update_SF(result, width);
 }
 
 #endif
