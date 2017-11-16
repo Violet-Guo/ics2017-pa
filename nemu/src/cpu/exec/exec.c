43f59b83e6db310da8a00965b563a517e863f256
tracer-ics2017 <tracer@njuics.org>
2017-11-16 05:54:25 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  05:54:25 up  2:59,  4 users,  load average: 0.10, 0.06, 0.01 140a5a872111c2901bfc646a360b81ecfcd16f2e

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -86,7 +86,7 @@ opcode_entry opcode_table [512] = {
   /* 0x2c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x30 */	EMPTY, IDEX(G2E, xor), EMPTY, EMPTY,
   /* 0x34 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x38 */	EMPTY, IDEX(G2E, cmp), EMPTY, IDEX(E2G, cmp),
+  /* 0x38 */	IDEXW(G2E, cmp, 1), IDEX(G2E, cmp), EMPTY, IDEX(E2G, cmp),
   /* 0x3c */	EMPTY, IDEX(I2a, cmp), EMPTY, EMPTY,
   /* 0x40 */	IDEX(r, inc), IDEX(r, inc),  EMPTY, IDEX(r, inc),
   /* 0x44 */	EMPTY, EMPTY, EMPTY, EMPTY,
