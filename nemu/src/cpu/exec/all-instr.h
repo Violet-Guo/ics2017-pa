21674bed342b7ab190d33266e893830aa14a022c
Violet-Guo <605654408@qq.com>
2017-11-16 12:27:21 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:27:21 up  5:25,  5 users,  load average: 0.00, 0.00, 0.00 1de59292c935c3c55b2dc3fa01a3ea289fb2b90

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -24,6 +24,7 @@ make_EHelper(and);
 make_EHelper(or);
 make_EHelper(shl);
 make_EHelper(sar);
+make_EHelper(not);
 
 make_EHelper(cmp);
 
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -51,7 +51,7 @@ make_group(gp2,
 
   /* 0xf6, 0xf7 */
 make_group(gp3,
-    IDEX(test_I, test), EMPTY, EMPTY, EMPTY,
+    IDEX(test_I, test), EMPTY, EX(not), EMPTY,
     EMPTY, EMPTY, EMPTY, EMPTY)
 
   /* 0xfe */
