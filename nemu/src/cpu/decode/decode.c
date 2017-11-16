5a1795235937ac46a4d730f67f1c554d75122775
tracer-ics2017 <tracer@njuics.org>
2017-11-16 04:47:47 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  04:47:47 up  1:53,  4 users,  load average: 0.05, 0.02, 0.00 b3c4dfeabbc03d2b95ce51db7d079f56191d5b0f

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,7 +38,8 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-	op->simm = instr_fetch(eip, op->width);
+  uint32_t value = instr_fetch(eip, op->width);
+  rtl_sext(&op->imm, &value, op->width);
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
