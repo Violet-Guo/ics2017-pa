30fe3a384af6b96d315f673079d1b791326c2c84
tracer-ics2017 <tracer@njuics.org>
2017-11-16 12:13:25 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:13:25 up  5:11,  4 users,  load average: 0.00, 0.00, 0.00 535b6193355e618fca8a46975cc68eebdd9ed97d

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -23,6 +23,7 @@ make_EHelper(xor);
 make_EHelper(and);
 make_EHelper(or);
 make_EHelper(shl);
+make_EHelper(sar);
 
 make_EHelper(cmp);
 
--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -39,8 +39,10 @@ make_EHelper(or) {
 }
 
 make_EHelper(sar) {
-  TODO();
   // unnecessary to update CF and OF in NEMU
+	rtl_sar(&t2, &id_dest->val, &id_src->val);
+	operand_write(id_dest, &t2);
+	rtl_update_ZFSF(&t2, id_dest->width);
 
   print_asm_template2(sar);
 }
