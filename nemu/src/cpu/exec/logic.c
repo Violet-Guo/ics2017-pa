8c24c8abc267390ae2313ddc899d8e1ce70e8c29
Violet-Guo <605654408@qq.com>
2017-11-23 06:03:20 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  06:03:20 up  1:10,  5 users,  load average: 0.01, 0.01, 0.00 d3c20451a2c18e70fbbc7d00d96a99f7fecc78d2

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -11,6 +11,9 @@ make_EHelper(test) {
 make_EHelper(and) {
 	rtl_and(&t2, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t2);
+	rtl_update_ZFSF(&t2, id_dest->width);
+	rtl_set_CF(&tzero);
+	rtl_set_OF(&tzero);
   print_asm_template2(and);
 }
 
