0f0280f9364adee468657bca4d2df917cb961429
tracer-ics2017 <tracer@njuics.org>
2017-11-24 16:12:59 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:12:59 up  6:40,  4 users,  load average: 0.05, 0.06, 0.06 b218234b29496889d058a320e37f23bba8a21477

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -6,6 +6,9 @@ make_EHelper(mov) {
 }
 
 make_EHelper(push) {
+	if (id_dest->width == 1) {
+		id_dest->val = (int32_t)(int8_t)id_dest->val;
+	}
 	rtl_push(&id_dest->val);
   print_asm_template1(push);
 }
