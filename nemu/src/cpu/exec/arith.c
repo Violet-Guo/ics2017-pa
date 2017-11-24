b490951f29a9bce821376c5729889f5a8dc691ec
Violet-Guo <605654408@qq.com>
2017-11-24 16:03:38 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:03:38 up  6:31,  4 users,  load average: 0.01, 0.08, 0.04 b99f848f3458bbef85e66b283389007a8e1ce1e3

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -6,7 +6,7 @@ make_EHelper(add) {
 
   rtl_update_ZFSF(&t2, id_dest->width);
 
-  rtl_sltu(&t0, &t2, &id_src->val);
+  rtl_sltu(&t0, &t2, &id_dest->val);
   rtl_set_CF(&t0);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
