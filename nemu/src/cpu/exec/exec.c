459de81fdb5235264304f8e2d89dbaed04bf215d
Violet-Guo <605654408@qq.com>
2017-11-24 08:07:40 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  08:07:40 up 27 min,  4 users,  load average: 0.00, 0.00, 0.00 d5f5703ba786bc4d304a3e1eb21ac4001ca9dc77

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -61,8 +61,8 @@ make_group(gp4,
 
   /* 0xff */
 make_group(gp5,
-    EMPTY, EMPTY, EX(call_rm), EX(call),
-    EMPTY, EX(inc), EX(push), EMPTY)
+    EX(inc), EMPTY, EX(call_rm), EX(call),
+    EMPTY, EMPTY, EX(push), EMPTY)
 
   /* 0x0f 0x01*/
 make_group(gp7,
