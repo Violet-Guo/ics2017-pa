55a14854dbcec45dc877a15ed982f5c8bee0e4ff
Violet-Guo <605654408@qq.com>
2017-11-24 14:58:25 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:58:25 up  5:26,  4 users,  load average: 0.01, 0.03, 0.02 ad4513a2804ea15cacc03ea87645a96a8c96003b

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -66,6 +66,7 @@ make_EHelper(shr) {
 make_EHelper(setcc) {
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
+	printf("setcc t2 = %d\n", t2);
   operand_write(id_dest, &t2);
 
   print_asm("set%s %s", get_cc_name(subcode), id_dest->str);
