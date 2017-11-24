d0dd33e0be1332ce806819c9a68fac5cc20918a1
tracer-ics2017 <tracer@njuics.org>
2017-11-24 08:44:40 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  08:44:40 up  1:04,  4 users,  load average: 0.00, 0.02, 0.00 40d56282dc9306bb37d6fafc34b114c452039ca

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -52,7 +52,7 @@ make_group(gp2,
   /* 0xf6, 0xf7 */
 make_group(gp3,
     IDEX(test_I, test), EMPTY, EX(not), EMPTY,
-    EX(mul), EX(imul1), EMPTY, EX(idiv))
+    EX(mul), EX(imul1), EX(div), EX(idiv))
 
   /* 0xfe */
 make_group(gp4,
