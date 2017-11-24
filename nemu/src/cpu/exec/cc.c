37c752855f1d4c08d6900cc9c1d68c3fd3e9cd84
Violet-Guo <605654408@qq.com>
2017-11-24 16:56:11 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:56:11 up  7:24,  4 users,  load average: 0.00, 0.00, 0.00 8d66f6e6901c55b7de21eb30f2348a38da251ff

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -11,6 +11,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_L, CC_NL, CC_LE, CC_NLE
   };
 	printf("invert = %d\n", invert);
+	printf("subcode = %d\n", subcode);
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
