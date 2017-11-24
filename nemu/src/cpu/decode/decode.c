ed7600349fe0b4f2fd49f454fafbd348e852e330
Violet-Guo <605654408@qq.com>
2017-11-24 15:53:40 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  15:53:40 up  6:21,  4 users,  load average: 0.00, 0.00, 0.00 ee6671737a81e34ea2a6b714d0dcd15aeb84938a

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,7 +38,6 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-	/*
 	if (op->width == 4) {
 		op->simm = instr_fetch(eip, op->width);
 	}
@@ -46,13 +45,8 @@ static inline make_DopHelper(SI) {
 		op->simm = (int32_t)(int16_t)(uint16_t)instr_fetch(eip, op->width);
 	}
 	else {
-		op->simm = (int32_t)(int8_t)(uint8_t)instr_fetch(eip, op->width);
-	}*/
-	if (op->width == 4) {
-		op->simm = instr_fetch(eip, op->width);
-  } else {
-		op->simm = (int32_t) (int8_t) (uint8_t) instr_fetch(eip, op->width); 
-  }
+		op->simm = (int32_t)(int16_t)(int8_t)(uint8_t)instr_fetch(eip, op->width);
+	}
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
