5455e61bf118a5d65b97a6fe5405d04d62d68b76
tracer-ics2017 <tracer@njuics.org>
2017-11-25 16:27:48 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:27:48 up 19:21,  4 users,  load average: 0.08, 0.04, 0.01 9744fbc3801618184a8ce3ea48a8821d2284af76

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -41,7 +41,7 @@ static make_EHelper(name) { \
 
 /* 0x80, 0x81, 0x83 */
 make_group(gp1,
-    EX(add), EMPTY, EMPTY, EX(sbb),
+    EX(add), EX(or), EMPTY, EX(sbb),
     EX(and), EX(sub), EMPTY, EX(cmp))
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
