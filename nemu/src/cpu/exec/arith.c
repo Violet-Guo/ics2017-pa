c5d7b17043c5fd720ea7c55effdac0d08bd721b5
Violet-Guo <605654408@qq.com>
2017-11-26 04:32:55 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  04:32:55 up 21:11,  4 users,  load average: 0.00, 0.00, 0.00 e9d58464595cc2ad1d3141bbc38355c7ae497649

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -92,7 +92,24 @@ make_EHelper(dec) {
 }
 
 make_EHelper(neg) {
-  TODO();
+	if (!id_dest->val) {
+		rtl_set_CF(&tzero);
+	}
+	else {
+		rtl_addi(&t0, &tzero, 1);
+    rtl_set_CF(&t0);
+	}
+	rtl_add(&t0, &tzero, &id_dest->val);
+  t0 = -t0;
+  operand_write(id_dest, &t0);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(neg);
 }
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -51,7 +51,7 @@ make_group(gp2,
 
   /* 0xf6, 0xf7 */
 make_group(gp3,
-    IDEX(test_I, test), EMPTY, EX(not), EMPTY,
+    IDEX(test_I, test), EMPTY, EX(not), EX(neg),
     EX(mul), EX(imul1), EX(div), EX(idiv))
 
   /* 0xfe */
