7ede9d0f314be8e331a12859304f9daf47702066
Violet-Guo <605654408@qq.com>
2017-11-26 05:57:42 +0000

    add keybord

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -2,6 +2,9 @@
 #include <x86.h>
 
 #define RTC_PORT 0x48   // Note that this is not standard
+#define I8042_DATA_PORT 0x60
+#define I8042_STATUS_PORT 0x64
+
 static unsigned long boot_time;
 
 void _ioe_init() {
@@ -32,5 +35,10 @@ void _draw_sync() {
 }
 
 int _read_key() {
-  return _KEY_NONE;
+	uint8_t impresskey = inb(I8042_STATUS_PORT);
+  if (impresskey) {
+	  return inl(I8042_DATA_PORT);
+  } else {
+	  return _KEY_NONE;
+  }
 }
