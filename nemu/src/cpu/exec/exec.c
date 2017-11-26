cb8337bc988da233b9bbc82b1f7deffafa9368f3
tracer-ics2017 <tracer@njuics.org>
2017-11-26 04:34:00 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  04:34:00 up 21:12,  4 users,  load average: 0.00, 0.00, 0.00 ec9d6602fcf4db614fa50a11485e61fe9e33b9c2

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -74,7 +74,7 @@ make_group(gp7,
 opcode_entry opcode_table [512] = {
   /* 0x00 */	EMPTY, IDEX(G2E, add), EMPTY, IDEX(E2G, add),
   /* 0x04 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x08 */	EMPTY, IDEX(G2E, or), IDEXW(E2G, or, 1), EMPTY,
+  /* 0x08 */	EMPTY, IDEX(G2E, or), IDEXW(E2G, or, 1), IDEX(E2G, or),
   /* 0x0c */	EMPTY, IDEX(I2a, or), EMPTY, EX(2byte_esc),
   /* 0x10 */	EMPTY, EMPTY, EMPTY, IDEX(E2G, adc),
   /* 0x14 */	EMPTY, EMPTY, EMPTY, EMPTY,
