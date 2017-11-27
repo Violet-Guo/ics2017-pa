8c2042242b74b5207915726aa79b49d10dd25a15
tracer-ics2017 <tracer@njuics.org>
2017-11-27 04:39:06 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  04:39:06 up  1:51,  4 users,  load average: 0.24, 0.07, 0.02 96af4e38909c8d2429c6c0236da6efdd4002956

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,11 +13,23 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int map_NO = is_mmio(addr);
+	if (map_NO != -1) {
+		return mmio_read(addr, len, map_NO);
+	}
+	else {
+		return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+	}
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+	int map_NO = is_mmio(addr);
+	if (map_NO != -1) {
+		mmio_write(addr, len, data, map_NO);
+	}
+  else {
+		memcpy(guest_to_host(addr), &data, len);
+	}
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
