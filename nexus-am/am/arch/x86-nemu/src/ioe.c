f6d0962bc30cdc6506824414b076768038db0c32
Violet-Guo <605654408@qq.com>
2017-11-29 10:58:29 +0000

    ics2017 initialized

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -26,9 +26,12 @@ extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
-  }
+	//for (i = 0; i < _screen.width * _screen.height; i++) {
+  //  fb[i] = i;
+  //}
+	for (i = 0; i < h; i++) {
+		memcpy(fb + (y + i) * _screen.width + x, pixels + i * w, w * 4);
+	}
 }
 
 void _draw_sync() {
