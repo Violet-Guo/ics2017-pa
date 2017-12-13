320000ee1c328903acc3ee2ee29a2be0fe380b67
tracer-ics2017 <tracer@njuics.org>
2017-12-13 16:02:29 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:02:29 up  4:48,  4 users,  load average: 0.40, 0.23, 0.08 873cd5fe1519164b290651e6cf87758350353c38

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -83,7 +83,7 @@ static inline void restart() {
   /* Set the initial instruction pointer. */
   cpu.eip = ENTRY_START;
 	cpu.cs = 0x8;
-	//cpu.eflags.eflags_init = 0x2;
+	cpu.eflags.eflags_init = 0x2;
 
 #ifdef DIFF_TEST
   init_qemu_reg();
