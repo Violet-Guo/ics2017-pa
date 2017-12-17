072cc142dee2825e2389fa5b17f937414c63fe5b
tracer-ics2017 <tracer@njuics.org>
2017-12-17 13:55:27 +0000

    >  compile 0000007 violet Linux a96e2163b2c7 4.9.49-moby #1 SMP Fri Dec 8 13:40:02 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  13:55:27 up 23:56,  6 users,  load average: 0.11, 0.09, 0.03 21c20d51f956753bf4e13ba6fcf315c87a3ae60

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -159,8 +159,8 @@ static inline void rtl_push(const rtlreg_t* src1) {
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-	//*dest = vaddr_read(cpu.esp, 4);
-	*dest = paddr_read(cpu.esp, 4);
+	*dest = vaddr_read(cpu.esp, 4);
+	//*dest = paddr_read(cpu.esp, 4);
 	cpu.esp += 4;
 }
 
