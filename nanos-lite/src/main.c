c2d241623b32f85ca73147df6b82129e121e8190
Violet-Guo <605654408@qq.com>
2017-12-19 14:09:15 +0000

    can run pal, but have some problem about the dialogue

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -32,7 +32,8 @@ int main() {
 
   //uint32_t entry = loader(NULL, "/bin/text");
   //uint32_t entry = loader(NULL, "/bin/bmptest");
-  uint32_t entry = loader(NULL, "/bin/events");
+  //uint32_t entry = loader(NULL, "/bin/events");
+  uint32_t entry = loader(NULL, "/bin/pal");
 	//Log("entry : %d", entry);
 	((void (*)(void))entry)();
 
